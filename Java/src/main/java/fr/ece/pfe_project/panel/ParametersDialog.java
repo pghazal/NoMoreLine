08f9c99349037aa92e30567e2cd8cd34cd6bf1be
Pierre Ghazal <pg.ghazal@gmail.com>
2015-01-12 10:34:56 +0100

    Editing Algo

--- Java/src/main/java/fr/ece/pfe_project/panel/ParametersDialog.java
+++ Java/src/main/java/fr/ece/pfe_project/panel/ParametersDialog.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fr.ece.pfe_project.panel;
 
 import fr.ece.pfe_project.model.FrequentationJournaliere;
