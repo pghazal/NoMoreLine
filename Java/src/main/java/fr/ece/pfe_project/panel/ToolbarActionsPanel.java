7ac0c4378a7d136ac089e8c013954dac65af77a0
Samy DANIEL <sdaniel@ece.fr>
2015-01-16 14:36:28 +0100

    samy's code

--- Java/src/main/java/fr/ece/pfe_project/panel/ToolbarActionsPanel.java
+++ Java/src/main/java/fr/ece/pfe_project/panel/ToolbarActionsPanel.java
@@ -29,12 +29,10 @@ public class ToolbarActionsPanel extends javax.swing.JPanel implements ToolbarEn
         if (cameraStatus == true) {
             
             cameraState.setIcon(ComponentManager.getInstance().getComponentIconDefaults().getonLedIcon());
-            System.out.println("LED ON");
-            
+
         } else {
-            
             cameraState.setIcon(ComponentManager.getInstance().getComponentIconDefaults().getoffLedIcon());
-            System.out.println("LED OFF");
+
         }
 
     }
