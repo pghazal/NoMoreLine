4e542ba9679181935ce530a6c7d8293fb847fee3
Pierre Ghazal <pg.ghazal@gmail.com>
2015-01-16 15:53:30 +0100

    JTable : color bkgd 1 ligne sur 2

--- Java/src/main/java/fr/ece/pfe_project/panel/ListPanel.java
+++ Java/src/main/java/fr/ece/pfe_project/panel/ListPanel.java
@@ -3,15 +3,18 @@ package fr.ece.pfe_project.panel;
 import fr.ece.pfe_project.editor.CameraCellEditor;
 import fr.ece.pfe_project.model.Camera;
 import fr.ece.pfe_project.model.Comptoir;
-import fr.ece.pfe_project.model.Employee;
 import fr.ece.pfe_project.model.FrequentationJournaliere;
 import fr.ece.pfe_project.model.ListingVols;
 import fr.ece.pfe_project.panel.MainPanel.FaceDetectorListener;
 import fr.ece.pfe_project.panel.MainPanel.ToolbarsListener;
+import static fr.ece.pfe_project.panel.ParametersDialog.msgbox;
 import fr.ece.pfe_project.renderer.CameraCellRenderer;
 import fr.ece.pfe_project.tablemodel.MyTableModel;
 import fr.ece.pfe_project.utils.GlobalVariableUtils;
 import fr.ece.pfe_project.widget.CameraCellComponent;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -22,12 +25,19 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.DateFormatSymbols;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.DefaultCellEditor;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField;
 import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
 import org.jdatepicker.ComponentManager;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -46,9 +56,100 @@ public class ListPanel extends javax.swing.JPanel implements FaceDetectorThread.
         public void changeCameraStatus(boolean cameraStatus);
     }
 
+    SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+
+    public class DateEditor extends DefaultCellEditor {
+
+        private final JFormattedTextField textField;
+
+        public DateEditor() {
+            super(new JFormattedTextField(f));
+
+            textField = (JFormattedTextField) super.getComponent();
+            textField.setFont(new Font(textField.getFont().getName(), Font.PLAIN, 11));
+        }
+
+        @Override
+        public Component getTableCellEditorComponent(JTable table,
+                Object value,
+                boolean isSelected,
+                int row,
+                int column) {
+
+            if (value instanceof Date) {
+
+                String strDate = f.format((Date) value);
+                this.textField.setText(strDate);
+            }
+
+            return this.textField;
+        }
+
+        @Override
+        public Object getCellEditorValue() {
+
+            try {
+                if (textField.getText().trim().length() == 0) {
+                    throw new ParseException("", 0);
+                }
+
+                java.util.Date utilDate = f.parse(textField.getText().trim());
+
+                return utilDate;
+            } catch (ParseException e) {
+                msgbox("Format date invalide. Format accepté : jj/mm/aaaa");
+
+                return textField.getText();
+            }
+        }
+    }
+
+    public class DateRenderer extends DefaultTableCellRenderer {
+
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value,
+                boolean isSelected, boolean hasFocus, int row, int col) {
+            super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, col);
+
+            if (value instanceof Date) {
+                String strDate = f.format((Date) value);
+                this.setText(strDate);
+            }
+
+            this.setBackground(row % 2 == 0 ? Color.LIGHT_GRAY : Color.WHITE);
+
+            if (isSelected) {
+                this.setBackground(Color.BLUE);
+                this.setForeground(Color.WHITE);
+            } else {
+                this.setForeground(Color.BLACK);
+            }
+
+            return this;
+        }
+    }
+
+    public class MyDefaultRenderer extends DefaultTableCellRenderer {
+
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+            final Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+            c.setBackground(row % 2 == 0 ? Color.LIGHT_GRAY : Color.WHITE);
+
+            if (isSelected) {
+                c.setBackground(Color.BLUE);
+                c.setForeground(Color.WHITE);
+            } else {
+                c.setForeground(Color.BLACK);
+            }
+
+            return c;
+        }
+    }
+
     private final Comptoir comptoirs[];
     private final Camera cameras[];
-    private final Employee employees[];
     private final ListingVols listingVols[];
     private boolean isCameraActive;
 
@@ -95,19 +196,19 @@ public class ListPanel extends javax.swing.JPanel implements FaceDetectorThread.
         };
 
         //cameras[1].setState(Camera.CAMERA_STATE.ALERT); // SET THE 2ND CAMERA AS DECTECTING CROWD
-        employees = new Employee[]{
-            new Employee(), new Employee(),
-            new Employee()
-        };
-
         isCameraActive = false;
-        // isRefreshBoutonActive = false;
         refreshButton.addActionListener(this);
-        itemsTable.setDefaultRenderer(Camera.class, new CameraCellRenderer());
-        itemsTable.setDefaultEditor(Camera.class, new CameraCellEditor());
+        CameraButton.addActionListener(this);
 
         this.itemsTable.setModel(new MyTableModel());
 
+        itemsTable.setDefaultRenderer(Integer.class, new MyDefaultRenderer());
+        itemsTable.setDefaultRenderer(String.class, new MyDefaultRenderer());
+        itemsTable.setDefaultRenderer(Long.class, new MyDefaultRenderer());
+        itemsTable.setDefaultRenderer(Date.class, new DateRenderer());
+        itemsTable.setDefaultRenderer(Camera.class, new CameraCellRenderer());
+
+        itemsTable.setDefaultEditor(Date.class, new DateEditor());
     }
 
     @Override
@@ -132,7 +233,6 @@ public class ListPanel extends javax.swing.JPanel implements FaceDetectorThread.
                 setCameraButtonVisibility(true);
                 itemsTable.setRowHeight(new CameraCellComponent().getPreferredSize().height);
                 model.setData(cameras, false);
-                CameraButton.addActionListener(this);
                 break;
             case EXCELROW:
                 setVisibilityRefresh(false);
@@ -147,7 +247,7 @@ public class ListPanel extends javax.swing.JPanel implements FaceDetectorThread.
                 setVisibility(false);
                 setCameraButtonVisibility(false);
                 itemsTable.setRowHeight(16);
-                //listingVols.addActionListener(this);
+
                 //Fonction à lancer lors du clique bouton: listingVolsrecup
                 if (!testConnexion()) {
                     model.setData((ListingVols[]) listingVolsrecup().toArray(new ListingVols[0]), false);
@@ -202,15 +302,15 @@ public class ListPanel extends javax.swing.JPanel implements FaceDetectorThread.
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
-        if(e.getSource() == CameraButton){
+
+        if (e.getSource() == CameraButton) {
             if (isCameraActive == true) {
-            //On désactive les caméras 
-            CameraButton.setIcon(ComponentManager.getInstance().getComponentIconDefaults().getgreenCameraIcon());
-            //  toolbarChangeStatusCamera.changeCameraStatus(isCameraActive);
-            cameraInterface(!isCameraActive);
-            toolbarsListener.changeCameraStatus(isCameraActive);
-            // CameraButton.setText("Activer caméra");
+                //On désactive les caméras 
+                CameraButton.setIcon(ComponentManager.getInstance().getComponentIconDefaults().getgreenCameraIcon());
+                //  toolbarChangeStatusCamera.changeCameraStatus(isCameraActive);
+                cameraInterface(!isCameraActive);
+                toolbarsListener.changeCameraStatus(isCameraActive);
+                // CameraButton.setText("Activer caméra");
             } else //On change le label du bouton (de "activer caméra" à "désactiver caméra) et sa couleur
             {
                 CameraButton.setIcon(ComponentManager.getInstance().getComponentIconDefaults().getredCameraIcon());
@@ -220,8 +320,6 @@ public class ListPanel extends javax.swing.JPanel implements FaceDetectorThread.
                 cameraInterface(!isCameraActive);
             }
         }
-
-        
 
         if (e.getSource() == refreshButton) {
 
@@ -430,6 +528,8 @@ public class ListPanel extends javax.swing.JPanel implements FaceDetectorThread.
         itemsTable.setAutoCreateRowSorter(true);
         itemsTable.setModel(new MyTableModel());
         itemsTable.setFillsViewportHeight(true);
+        itemsTable.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        itemsTable.setShowHorizontalLines(false);
         itemsTable.setShowVerticalLines(false);
         itemsTable.getTableHeader().setReorderingAllowed(false);
         jScrollPane1.setViewportView(itemsTable);
--- Java/src/main/java/fr/ece/pfe_project/renderer/CameraCellRenderer.java
+++ Java/src/main/java/fr/ece/pfe_project/renderer/CameraCellRenderer.java
@@ -2,6 +2,7 @@ package fr.ece.pfe_project.renderer;
 
 import fr.ece.pfe_project.model.Camera;
 import fr.ece.pfe_project.widget.CameraCellComponent;
+import java.awt.Color;
 import java.awt.Component;
 import javax.swing.JTable;
 import javax.swing.table.TableCellRenderer;
@@ -22,6 +23,7 @@ public class CameraCellRenderer implements TableCellRenderer {
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
         Camera camera = (Camera) value;
         panel.updateData(camera, isSelected, table);
+        panel.setBackground(row % 2 == 0 ? Color.LIGHT_GRAY : Color.WHITE);
         return panel;
     }
 }
